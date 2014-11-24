75cc8b9fda04fa63a8dd07b018a8dea47772146f
gigasquid <cmeier@gigasquidsoftware.com>
2014-11-23 21:35:55 -0500

    add instructions on how to get out of the repl without having to ctl-c

--- target.py
+++ target.py
@@ -61,6 +61,7 @@ class ReplFn(NativeFn):
         import pixie.vm.persistent_vector as vector
 
         print "Pixie 0.1 - Interactive REPL"
+        print ":exit-repl to quit"
         print "(" + platform.name + ", " + platform.cc + ")"
         print "----------------------------"
 
