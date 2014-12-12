4a208fd0976dd4250929b00e4d3e1970cc8fd1dd
Timothy Baldridge <tbaldridge@gmail.com>
2014-12-12 14:38:33 -0700

    add preloading of all .pxi files

--- target_preload.py
+++ target_preload.py
@@ -1,12 +1,17 @@
-from target import entry_point, load_stdlib, init_load_path, LOAD_PATHS, load_path
+from target import entry_point, load_stdlib, init_load_path, LOAD_PATHS, load_path, BatchModeFn
+from pixie.vm.atom import Atom
 from pixie.vm.persistent_vector import EMPTY as EMPTY_VECTOR
+from pixie.vm.symbol import symbol
+from pixie.vm.code import intern_var
 
 import pixie.vm.rt as rt
 rt.init()
 
 load_path.set_root(rt.wrap(u"./"))
-LOAD_PATHS.set_root(EMPTY_VECTOR.conj(rt.wrap(u"./")))
+LOAD_PATHS.set_root(Atom(EMPTY_VECTOR.conj(rt.wrap(u"./"))))
 load_stdlib()
+
+BatchModeFn(["pixie/preload.pxi"]).invoke([])
 
 def target(*args):
     import pixie.vm.rt as rt
